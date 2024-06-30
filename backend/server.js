129037f138c60bb70421115297aa50643bf177c3
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-06-30 11:22:25 +0530

    added scripts to run backend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.post("/api/generate-post", async (req, res) => {
   const { prompt } = req.body;
 
   try {
-    const response = await axios(
+    const response = await axios.post(
       `https://api.openai.com/v1/completions`,
       {
         model: `text-davinci-003`,
@@ -25,6 +25,7 @@ app.post("/api/generate-post", async (req, res) => {
       },
       {
         headers: {
+          "Content-Type": "application/json",
           Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
         },
       }
